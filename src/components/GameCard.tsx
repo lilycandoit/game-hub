9a0eaaf00d4094608b85e5cb1f1963bb26bea84d
lilycandoit <hueduong288@gmail.com>
2023-07-03 17:30:47 +0800

    building game card

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,22 @@
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+
+interface Props {
+  game: Game;
+}
+
+const GameCard = ({ game }: Props) => {
+  return (
+    <Card
+      borderRadius={10}
+      overflow="hidden" //because the image bigger the container so the borderradius works for the lower corners only
+    >
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
