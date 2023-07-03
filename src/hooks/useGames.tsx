56211e1fa24fb1feb2d2dc017c19a7bfb83c89bc
lilycandoit <hueduong288@gmail.com>
2023-07-03 22:37:52 +0800

    Displaying Critic Scores

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -13,6 +13,7 @@ export interface Game {
   name: string;
   background_image: string;
   parent_platforms: { platform: Platform }[]//it is an array of objects which each object has platform type of Platform (declared in interface above)
+  metacritic: number;
 }
 
 interface FetchGamesResponse {
