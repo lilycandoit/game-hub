9d78e86048b3153c2109b3107036b30f00c477f2
lilycandoit <hueduong288@gmail.com>
2023-07-10 17:22:14 +0800

    Add emojis

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,8 +1,9 @@
-import { Game } from "../hooks/useGames";
-import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
-import PlatformIconList from "./PlatformIconList";
-import CriticScore from "./CriticScore";
-import getCroppedImageUrl from "../services/image-url";
+import { Game } from '../hooks/useGames';
+import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react';
+import PlatformIconList from './PlatformIconList';
+import CriticScore from './CriticScore';
+import getCroppedImageUrl from '../services/image-url';
+import Emoji from './Emoji';
 
 interface Props {
   game: Game;
@@ -13,13 +14,16 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent='space-between'>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Heading fontSize='2xl'>
+          {game.name}
+          <Emoji rating={game.rating_top} />
+        </Heading>
       </CardBody>
     </Card>
   );
--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -14,6 +14,7 @@ export interface Game {
   background_image: string;
   parent_platforms: { platform: Platform }[]; //it is an array of objects which each object has platform type of Platform (declared in interface above)
   metacritic: number;
+  rating_top: number;
 }
 
 const useGames = (gameQuery: GameQuery) =>
