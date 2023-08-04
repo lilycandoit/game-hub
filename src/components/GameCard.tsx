4b277bdede497c1915e4fc40aa122999016538fe
lilycandoit <hueduong288@gmail.com>
2023-08-04 08:31:50 +0800

    Fetching Platforms_part2(bug)

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,6 +10,7 @@ interface Props {
 }
 
 const GameCard = ({ game }: Props) => {
+  console.log(game.parent_platforms)
   return (
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
