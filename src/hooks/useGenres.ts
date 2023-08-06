ea628e7d83c2f8281f9488fc65f86701f9d7c8ab
lilycandoit <hueduong288@gmail.com>
2023-08-06 18:13:21 +0800

    Refactoring-create a reusable API client

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
 import genres from '../data/genres';
-import apiClient, { FetchResponse } from '../services/api-client';
+import APIClient from '../services/api-client';
+
+const apiClient = new APIClient<Genre>('/genres')
 
 export interface Genre {
   id: number;
@@ -10,10 +12,7 @@ export interface Genre {
 
 const useGenres = () => useQuery({
   queryKey: ['genre'],
-  queryFn: () => 
-    apiClient
-      .get<FetchResponse<Genre>>('/genre')
-      .then(res => res.data),
+  queryFn: apiClient.getAll,
   staleTime: 24*60*60*1000, //24hours
   initialData: {count: genres.length, results: genres} // to save the data in the cache and staleTime is 24 hours, so no request will be sent to backend in 24hours, and we dont see the spinner of genres loading
 });
