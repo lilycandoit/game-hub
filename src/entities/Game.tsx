aec628f4d751c28cf5f7b4cdb6d3246f1515da5b
lilycandoit <hueduong288@gmail.com>
2023-08-24 00:10:01 +0800

    Building Game Attributes

--- src/entities/Game.tsx
+++ src/entities/Game.tsx
@@ -1,9 +1,13 @@
+import { Genre } from './Genre';
 import { Platform } from './Platform';
+import { Publisher } from './Publishers';
 
 export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres: Genre[];
+  publishers: Publisher[];
   description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform }[]; //it is an array of objects which each object has platform type of Platform (declared in interface above)
