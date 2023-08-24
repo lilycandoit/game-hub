7fcf73f2fd77d7a9ab75e654e6974644fb0fef16
lilycandoit <hueduong288@gmail.com>
2023-08-24 23:14:55 +0800

    Refectoring Entity interfaces

--- src/hooks/useScreenshots.ts
+++ src/hooks/useScreenshots.ts
@@ -1,16 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
-import APIClient from "../services/api-client";
-import { Screenshot } from "../entities/Screenshot";
+import { useQuery } from '@tanstack/react-query';
+import APIClient from '../services/api-client';
+import Screenshot from '../entities/Screenshot';
 
-
-
-const useScreenshots = (gameId: number)=> {
-  const apiClient = new APIClient<Screenshot>(`/games/${gameId}/screenshots`)
+const useScreenshots = (gameId: number) => {
+  const apiClient = new APIClient<Screenshot>(`/games/${gameId}/screenshots`);
 
   return useQuery({
-  queryKey: ['screenshots', gameId],
-  queryFn: apiClient.getAll
-})
-}
+    queryKey: ['screenshots', gameId],
+    queryFn: apiClient.getAll,
+  });
+};
 
 export default useScreenshots;
